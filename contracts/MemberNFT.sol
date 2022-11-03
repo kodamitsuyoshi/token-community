b8fe7f100ddd1af68beea6275a34105dff9a696e
Mitsuyoshi Koda <3nan.mkoda@gmail.com>
2022-11-04 01:52:19 +0900

    add test

--- contracts/MemberNFT.sol
+++ contracts/MemberNFT.sol
@@ -1,6 +1,75 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.9;
 
-contract MemberNFT {
-    
+import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
+import "@openzeppelin/contracts/token/ERC721/extensions/ERC721URIStorage.sol";
+import "@openzeppelin/contracts/token/ERC721/extensions/ERC721Enumerable.sol";
+import "@openzeppelin/contracts/access/Ownable.sol";
+import "@openzeppelin/contracts/utils/Counters.sol";
+
+contract MemberNFT is ERC721URIStorage, ERC721Enumerable, Ownable {
+    constructor() ERC721("MemberNFT", "MEM") {}
+
+    /**
+     * @dev Hook that is called before any token transfer. This includes minting
+     * and burning.
+     *
+     * Calling conditions:
+     *
+     * - When `from` and `to` are both non-zero, ``from``'s `tokenId` will be
+     * transferred to `to`.
+     * - When `from` is zero, `tokenId` will be minted for `to`.
+     * - When `to` is zero, ``from``'s `tokenId` will be burned.
+     * - `from` and `to` are never both zero.
+     *
+     * To learn more about hooks, head to xref:ROOT:extending-contracts.adoc#using-hooks[Using Hooks].
+     */
+    function _beforeTokenTransfer(
+        address from,
+        address to,
+        uint256 tokenId
+    ) internal override(ERC721, ERC721Enumerable) {
+        super._beforeTokenTransfer(from, to, tokenId);
+    }
+
+    /**
+     * @dev Destroys `tokenId`.
+     * The approval is cleared when the token is burned.
+     *
+     * Requirements:
+     *
+     * - `tokenId` must exist.
+     *
+     * Emits a {Transfer} event.
+     */
+    function _burn(uint256 tokenId)
+        internal
+        override(ERC721, ERC721URIStorage)
+    {
+        super._burn(tokenId);
+    }
+
+    /**
+     * @dev See {IERC165-supportsInterface}.
+     */
+    function supportsInterface(bytes4 interfaceId)
+        public
+        view
+        override(ERC721, ERC721Enumerable)
+        returns (bool)
+    {
+        return super.supportsInterface(interfaceId);
+    }
+
+    /**
+     * @dev See {IERC721Metadata-tokenURI}.
+     */
+    function tokenURI(uint256 tokenId)
+        public
+        view
+        override(ERC721, ERC721URIStorage)
+        returns (string memory)
+    {
+        return super.tokenURI(tokenId);
+    }
 }
---
+++ test/MemberNFT.js
@@ -0,0 +1,13 @@
+const {expect} = require("Chai");
+
+
+describe("MemberNFTContract",function(){
+    it ("トークンと名前とシンボルがセットされるべき",async function(){
+        expect().to.equal(0);
+
+    });
+    it ("デプロイアドレスがownerセットされるべき",async function(){
+        expect().to.equal(0);
+
+    });
+});
