8c1709221244828329195c1900c2756efed4a499
Mitsuyoshi Koda <3nan.mkoda@gmail.com>
2022-11-08 14:52:33 +0900

    add env.sample

--- hardhat.config.js
+++ hardhat.config.js
@@ -4,4 +4,10 @@ require("@nomiclabs/hardhat-waffle");
  */
 module.exports = {
   solidity: "0.8.9",
+  networks:{
+    goerli:{
+      url:`https://eth-goerli.g.alchemy.com/v2/${process.env.ALCHEMY_API_KEY}`,
+      accounts:[process.env.PRIVATE_KEY]
+    }
+  }
 };
